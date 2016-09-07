d10e8f2cf8a58cb582cc4ab0328e64923e965be7
Tim Diekmann <tdiekmann@techfak.uni-bielefeld.de>
2016-09-07 13:16:47 +0200

    Application overhaul

--- Sources/Application.swift
+++ Sources/Application.swift
@@ -18,112 +18,108 @@
 
 import CGtk
 
-public class Application: Object {
-	internal var application: UnsafeMutablePointer<GtkApplication> {
-		get {
-			return unsafeBitCast(object, to: UnsafeMutablePointer<GtkApplication>.self)
-		}
-	}
- 	internal var gApplication: UnsafeMutablePointer<GApplication> {
-		get {
-			return unsafeBitCast(object, to: UnsafeMutablePointer<GApplication>.self)
-		}
-	}
-	public convenience init(id: String, flags: ApplicationFlags = .None) {
-		self.init(gtk_application_new(id, flags.value())!)
-	}
-	
-	public func add(window: Window) {
-		gtk_application_add_window(application, window.window)
-	}
-	public func remove(window: Window) {
-		gtk_application_remove_window(application, window.window)
-	}
-	public var windows: [Window] {get{
-		return Array<Window>(from: gtk_application_get_windows(application))
-	}}
-	public func getWindow(id: UInt32) -> Window? {
-		return Window(gtk_application_get_window_by_id(application, id))
-	}
-	
-	public func run() {
-		g_application_run(gApplication, CommandLine.argc, CommandLine.unsafeArgv)
-		g_object_unref(application)
-	}
-	public var prefersAppMenu: Bool {get{
-		return gtk_application_prefers_app_menu(application) != 0
-	}}
-	public func hold() {
-		g_application_hold(gApplication)
-	}
-	public func release() {
-		g_application_release(gApplication)
-	}
-	public func quit() {
-		g_application_quit(gApplication)
-	}
-	
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	/// Properties
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	var applicationId: String {
-		get {
-			return String(cString: g_application_get_application_id(gApplication))
-		} set {
-			g_application_set_application_id(gApplication, newValue)
-		}
-	}
-	var activeWindow: Window {
-		get {
-			return Window(gtk_application_get_active_window(application))
-		}
-	}
-	var inactivityTimeout: UInt32 {
-		get {
-			return g_application_get_inactivity_timeout(gApplication)
-		} set {
-			g_application_set_inactivity_timeout(gApplication, newValue)
-		}
-	}
-	var resourceBasePath: String {
-		get {
-			return String(cString: g_application_get_resource_base_path(gApplication))
-		} set {
-			g_application_set_resource_base_path(gApplication, newValue)
-		}
-	}
-	var isRegistered: Bool {
-		get {
-			return g_application_get_is_registered(gApplication) != 0
-		}
-	}
-	var isRemote: Bool {
-		get {
-			return g_application_get_is_remote(gApplication) != 0
-		}
-	}
-	
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	/// Signals
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	public typealias Activated = (Application)->Void
-	public lazy var activated: Signal<Activated,(@convention(c)(UnsafeMutablePointer<GtkApplication>, UnsafeRawPointer) -> Void)>
-	= Signal(object: self, signal: "activate", cClosure: { instance, data in
-		let function = Unmanaged<SignalData>.fromOpaque(data).takeUnretainedValue().function as! Activated
-		function(Application(instance))
-	})
-	
-	public typealias WindowAdded = (Application, Window)->Void
-	public lazy var windowAdded: Signal<WindowAdded,(@convention(c)(UnsafeMutablePointer<GtkApplication>, UnsafeMutablePointer<GtkWindow>, UnsafeRawPointer) -> Void)>
-	= Signal(object: self, signal: "window-added", cClosure: { instance, window, data in
-		let function = Unmanaged<SignalData>.fromOpaque(data).takeUnretainedValue().function as! WindowAdded
-		function(Application(instance), Window(window))
-	})
-	
-	public typealias WindowRemoved = (Application, Window)->Void
-	public lazy var windowRemoved: Signal<WindowRemoved,(@convention(c)(UnsafeMutablePointer<GtkApplication>, UnsafeMutablePointer<GtkWindow>, UnsafeRawPointer) -> Void)>
-	= Signal(object: self, signal: "window-removed", cClosure: { instance, window, data in
-		let function = Unmanaged<SignalData>.fromOpaque(data).takeUnretainedValue().function as! WindowRemoved
-		function(Application(instance), Window(window))
-	})
+public protocol GApplicationProtocol {
+  typealias Pointer = UnsafeMutablePointer<GApplication>
+}
+public protocol ApplicationProtocol: GApplicationProtocol {
+  typealias Pointer = UnsafeMutablePointer<GtkApplication>
+}
+public struct Application: ApplicationProtocol, Object {
+  public var underlying: UnsafeMutablePointer<GtkApplication>
+  
+  init(_ ptr: UnsafeMutableRawPointer) {
+    underlying = unsafeBitCast(ptr, to: Pointer.self)
+  }
+  public init(id: String, flags: ApplicationFlags = .None) {
+    self.init(unsafeBitCast(gtk_application_new(id, flags.value), to: Pointer.self))
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Functions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public extension Object where Self: ApplicationProtocol {
+  public func add(window: Window) {
+    gtk_application_add_window(underlying, window.underlying)
+  }
+  public func remove(window: Window) {
+    gtk_application_remove_window(underlying, window.underlying)
+  }
+  public var windows: [Window] {
+    return Array<Window>(from: gtk_application_get_windows(underlying))
+  }
+  public func getWindow(id: UInt32) -> Window? {
+    return Window(gtk_application_get_window_by_id(underlying, id))
+  }
+}
+public extension Object where Self: GApplicationProtocol {
+  public func run() {
+    g_application_run(underlying, CommandLine.argc, CommandLine.unsafeArgv)
+    g_object_unref(underlying)
+  }
+  public func hold() {
+    g_application_hold(underlying)
+  }
+  public func release() {
+    g_application_release(underlying)
+  }
+  public func quit() {
+    g_application_quit(underlying)
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Properties
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public extension Object where Self: ApplicationProtocol {
+  public var prefersAppMenu: Bool {
+    return gtk_application_prefers_app_menu(underlying) != 0
+  }
+  var activeWindow: Window {
+    return Window(gtk_application_get_active_window(underlying))
+  }
+}
+public extension Object where Self: GApplicationProtocol {
+  var applicationId: String {
+    get {
+      return String(cString: g_application_get_application_id(underlying))
+    } set {
+      g_application_set_application_id(underlying, newValue)
+    }
+  }
+  var inactivityTimeout: UInt32 {
+    get {
+      return g_application_get_inactivity_timeout(underlying)
+    } set {
+      g_application_set_inactivity_timeout(underlying, newValue)
+    }
+  }
+  var resourceBasePath: String {
+    get {
+      return String(cString: g_application_get_resource_base_path(underlying))
+    } set {
+      g_application_set_resource_base_path(underlying, newValue)
+    }
+  }
+  var isRegistered: Bool {
+    return g_application_get_is_registered(underlying) != 0
+  }
+  var isRemote: Bool {
+    return g_application_get_is_remote(underlying) != 0
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Signals
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public extension Object where Self: ApplicationProtocol {
+  var activated: ActivatedSignal {
+    return ActivatedSignal(instance: underlying)
+  }
+  var windowAdded: WindowAddedSignal {
+    return WindowAddedSignal(instance: underlying)
+  }
+  var windowRemoved: WindowRemovedSignal {
+    return WindowRemovedSignal(instance: underlying)
+  }
 }
